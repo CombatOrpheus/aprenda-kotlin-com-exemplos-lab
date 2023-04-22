c12be862ce7c598a7458c0bab163b1786278b9c6
Gabriel Oliveira <santosgabriel28@hotmail.com>
2023-04-22 15:05:39 -0300

    Adding some content and attributes to the basic classes

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,70 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel(val proporcaoXp: Double) {
+    BASICO(0.5),
+    INTERMEDIARIO(1.0),
+    DIFICIL(2.0)
+}
 
-class Usuario
+data class Usuario(
+    val nome: String,
+    val conteudosInscritos: Set<ConteudoEducacional> = mutableSetOf<ConteudoEducacional>(),
+    val conteudosCompletos: Set<ConteudoEducacional> = mutableSetOf<ConteudoEducacional>()
+) {
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudosInscritos.plus(conteudo)
+    }
+
+    fun adicionarConteudos(conteudos: List<ConteudoEducacional>) {
+        conteudosInscritos.plus(conteudos)
+    }
+
+    fun completarConteudo(conteudo: ConteudoEducacional) {
+        if (conteudosInscritos.contains(conteudo)) {
+            conteudosCompletos.plus(conteudo)
+            conteudosInscritos.minus(conteudo)
+        } else {
+            throw MatchException("Usuário não está inscrito neste conteudo", IllegalArgumentException())
+        }
+    }
+    fun calcularXp(): Double {
+        return conteudosCompletos.map { it.xpConteudo() }.sum()
+    }
+
+}
+
+data class ConteudoEducacional(
+    var nome: String,
+    val duracao: Int = 60,
+    val nivel: Nivel = Nivel.BASICO,
+) {
+    fun xpConteudo(): Double {
+        return duracao * nivel.proporcaoXp
+    }
+}
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+    val xpFormacao = conteudos.map { it.duracao }.sum()
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudoBasico = listOf("Introdução à Programação", "SQL Básico", "APIs Web").map { ConteudoEducacional(it) }
+    val conteudoIntermediario =
+        listOf("Laços de Repetição Java", "Armazenamento de Dados com o Docker", "Projetos Java com Gradle")
+            .map { ConteudoEducacional(it, duracao = 80, nivel = Nivel.INTERMEDIARIO) }
+    val conteudoAvancado =
+        listOf("Adicionando Segurança a uma API REST com Spring Security", "Docker Compose")
+            .map { ConteudoEducacional(it, duracao = 120, nivel = Nivel.DIFICIL) }
+
+    val usuarioJoao = Usuario("João Pedro", conteudoBasico.toSet())
+    val usuarioMaria = Usuario("Maria do Socorro",
+        setOf(conteudoBasico[0], conteudoIntermediario[0], conteudoAvancado[0]))
+    val usuarioRafaela = Usuario("Rafaela Paz", conteudoAvancado.toSet())
 }
